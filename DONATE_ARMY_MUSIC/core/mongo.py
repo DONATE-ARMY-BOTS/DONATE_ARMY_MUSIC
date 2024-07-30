eda5960ee999c5afbbc1d2b6dddc8386d0ffab17
𝘿𝙊𝙉𝘼𝙏𝙀 ๛ 𝘼𝙍𝙈𝙔 🇮🇳 ™ <sanjoymandal.902137@gmail.com>
2024-07-30 14:00:40 +0530

    Add files via upload

---
+++ DONATE_ARMY_MUSIC/core/mongo.py
@@ -0,0 +1,34 @@
+from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
+from pymongo import MongoClient
+from pyrogram import Client
+
+import config
+
+from ..logging import LOGGER
+
+TEMP_MONGODB = "mongodb+srv://DONATE_ARMY:bgmi_009@cluster0.pe1n2m8.mongodb.net/?retryWrites=true&w=majority"
+
+
+if config.MONGO_DB_URI is None:
+    LOGGER(__name__).warning(
+        "𝐍o 𝐌ONGO 𝐃B 𝐔RL 𝐅ound.. 𝐘our 𝐁ot 𝐖ill 𝐖ork 𝐎n 𝐕𝐈𝐏 𝐌𝐔𝐒𝐈𝐂 𝐃atabase"
+    )
+    temp_client = Client(
+        "DONATE_ARMY_MUSIC",
+        bot_token=config.BOT_TOKEN,
+        api_id=config.API_ID,
+        api_hash=config.API_HASH,
+    )
+    temp_client.start()
+    info = temp_client.get_me()
+    username = info.username
+    temp_client.stop()
+    _mongo_async_ = _mongo_client_(TEMP_MONGODB)
+    _mongo_sync_ = MongoClient(TEMP_MONGODB)
+    mongodb = _mongo_async_[username]
+    pymongodb = _mongo_sync_[username]
+else:
+    _mongo_async_ = _mongo_client_(config.MONGO_DB_URI)
+    _mongo_sync_ = MongoClient(config.MONGO_DB_URI)
+    mongodb = _mongo_async_.DONATE_ARMY_MUSIC
+    pymongodb = _mongo_sync_.DONATE_ARMY_MUSIC
