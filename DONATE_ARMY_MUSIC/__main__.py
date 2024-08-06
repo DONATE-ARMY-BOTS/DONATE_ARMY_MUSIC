d31245ee94e3616cfe238ebb8ea27cdb176be3df
𝘿𝙊𝙉𝘼𝙏𝙀 ๛ 𝘼𝙍𝙈𝙔 🇮🇳 ™ <sanjoymandal.902137@gmail.com>
2024-08-06 20:11:18 +0530

    Add files via upload

--- DONATE_ARMY_MUSIC/__main__.py
+++ DONATE_ARMY_MUSIC/__main__.py
@@ -1,17 +1,18 @@
 import asyncio
 import importlib
-
+from sys import argv
 from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from config import BANNED_USERS
-from DONATE_ARMY_MUSIC import HELPABLE, LOGGER, app, userbot
+from DONATE_ARMY_MUSIC import LOGGER, app, userbot
 from DONATE_ARMY_MUSIC.core.call import VIP
 from DONATE_ARMY_MUSIC.misc import sudo
 from DONATE_ARMY_MUSIC.plugins import ALL_MODULES
-from DONATE_ARMY_MUSIC.plugins.tools.clone import restart_bots
 from DONATE_ARMY_MUSIC.utils.database import get_banned_users, get_gbanned
-
+from config import BANNED_USERS
+from DONATE_ARMY_MUSIC import telethn
+from DONATE_ARMY_MUSIC.plugins.tools.clone import restart_bots
 
 async def init():
     if (
@@ -21,12 +22,9 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER(__name__).error(
-            "𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 V2 𝐒𝐞𝐬𝐬𝐢𝐨𝐧🤬"
-        )
-
+        LOGGER(__name__).error("𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 V2 𝐒𝐞𝐬𝐬𝐢𝐨𝐧🤬")
+        
     await sudo()
-
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -39,35 +37,25 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        imported_module = importlib.import_module(all_module)
-
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-                if imported_module.__MODULE__.lower() not in HELPABLE:
-                    HELPABLE[imported_module.__MODULE__.lower()] = imported_module
-                else:
-                    raise Exception(
-                        f"Can't have two plugins with same name! '{imported_module.__MODULE__}' Please Change One"
-                    )
-
+        importlib.import_module("DONATE_ARMY_MUSIC.plugins" + all_module)
     LOGGER("DONATE_ARMY_MUSIC.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
-
+    await restart_bots()
     await userbot.start()
-
     await VIP.start()
     await VIP.decorators()
-    await restart_bots()
-    LOGGER("DONATE_ARMY_MUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    LOGGER("DONATE_ARMY_MUSIC").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗗𝗢𝗡𝗔𝗧𝗘_𝗔𝗥𝗠𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝"
+    )
     await idle()
-
+    if len(argv) not in (1, 3, 4):
+        await telethn.disconnect()
+    else:
+        await telethn.run_until_disconnected()
+                
     await app.stop()
     await userbot.stop()
-
-    LOGGER("DONATE_ARMY_MUSIC").info(
-        "                 ╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝"
-    )
-
+    LOGGER("DONATE_ARMY_MUSIC").info("                 ╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗗𝗢𝗡𝗔𝗧𝗘_𝗔𝗥𝗠𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    
 
 if __name__ == "__main__":
-
+    telethn.start(bot_token=config.BOT_TOKEN)
     asyncio.get_event_loop().run_until_complete(init())
